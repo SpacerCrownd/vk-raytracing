542db002c42dc7c807e8b59bdd1b80598c2e252c
Yun Qing Zhou <53042240+SpacerCrownd@users.noreply.github.com>
2026-05-28 20:16:53 +0200

    converting to draw image wip

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -5,20 +5,20 @@
 
 namespace app {
 Renderer::Renderer(int width, int height, const char* pAppName) : width(width), height(height),
-                                                                  m_mainWindow(width, height, pAppName),
-                                                                  m_vkCore(pAppName, m_mainWindow),
+                                                                  m_window(width, height, pAppName),
+                                                                  m_vkCore(pAppName, m_window),
                                                                   m_scene(),
                                                                   m_camera(glm::vec3(0.0f, 0.0f, 0.0f))
 {
-    m_mainWindow.AddOnKeyChanged([this](int key, int scancode, int action, int mods){
+    m_window.AddOnKeyChanged([this](int key, int scancode, int action, int mods){
         m_camera.OnKeyChanged(key, scancode, action, mods);
     });
 
-    m_mainWindow.AddOnMouseButtonChanged([this](int button, int action, int mods) {
-        m_camera.OnMouseButtonChanged(this->m_mainWindow.GetWindow(), button, action, mods);
+    m_window.AddOnMouseButtonChanged([this](int button, int action, int mods) {
+        m_camera.OnMouseButtonChanged(this->m_window.GetWindow(), button, action, mods);
     });
 
-    m_mainWindow.AddOnCursorPositionChanged([this](double x, double y) {
+    m_window.AddOnCursorPositionChanged([this](double x, double y) {
         m_camera.OnCursorPositionChanged(x, y);
     });
 }
@@ -40,7 +40,7 @@ void Renderer::MainLoop() {
     int frames = 0;
     float fpsTime = 0.0f;
     */
-    while (!glfwWindowShouldClose(m_mainWindow.GetWindow())) {
+    while (!glfwWindowShouldClose(m_window.GetWindow())) {
         PrepareFrameData();
         Draw();
         glfwPollEvents();
@@ -59,7 +59,11 @@ void Renderer::LoadShaders() {
 }
 
 void Renderer::CreateGraphicsPipeline() {
-    
+    //m_graphicsPipeline.emplace(m_vkCore.GetDevice().GetVkDevice(), m_window.GetWindow(), m_rasterShader->GetShaderModule());
+}
+
+void Renderer::PrepareFrameData() {
+
 }
 
 void Renderer::Draw() {
@@ -76,11 +80,23 @@ void Renderer::Draw() {
         .layerCount = 1,
     };
 
-    vk::ImageMemoryBarrier2 presentToWriteBarrier = {
+    vk::ImageMemoryBarrier2 toGeneralBarrier = {
         .srcStageMask = vk::PipelineStageFlagBits2::eTransfer,
         .srcAccessMask = vk::AccessFlagBits2::eMemoryRead,
         .dstStageMask = vk::PipelineStageFlagBits2::eTransfer,
         .dstAccessMask = vk::AccessFlagBits2::eTransferWrite,
+        .oldLayout = vk::ImageLayout::eUndefined,
+        .newLayout = vk::ImageLayout::eGeneral,
+        .srcQueueFamilyIndex = vk::QueueFamilyIgnored,
+        .dstQueueFamilyIndex = vk::QueueFamilyIgnored,
+        .subresourceRange = imageRange,
+    };
+
+    vk::ImageMemoryBarrier2 toTransferSrcBarrier = {
+        .srcStageMask = vk::PipelineStageFlagBits2::eTransfer,
+        .srcAccessMask = vk::AccessFlagBits2::eTransferWrite,
+        .dstStageMask = vk::PipelineStageFlagBits2::eBlit,
+        .dstAccessMask = vk::AccessFlagBits2::eMemoryRead,
         .oldLayout = vk::ImageLayout::eUndefined,
         .newLayout = vk::ImageLayout::eTransferDstOptimal,
         .srcQueueFamilyIndex = vk::QueueFamilyIgnored,
@@ -89,31 +105,27 @@ void Renderer::Draw() {
     };
 
     vk::ImageMemoryBarrier2 writeToPresentBarrier = {
-        .srcStageMask = vk::PipelineStageFlagBits2::eTransfer,
-        .srcAccessMask = vk::AccessFlagBits2::eTransferWrite,
-        .dstStageMask = vk::PipelineStageFlagBits2::eNone,
-        .dstAccessMask = vk::AccessFlagBits2::eMemoryRead,
-        .oldLayout = vk::ImageLayout::eTransferDstOptimal,
-        .newLayout = vk::ImageLayout::ePresentSrcKHR,
-        .srcQueueFamilyIndex = vk::QueueFamilyIgnored,
-        .dstQueueFamilyIndex = vk::QueueFamilyIgnored,
-        .subresourceRange = imageRange,
+        .srcStageMask = vk::PipelineStageFlagBits2::eBlit,
+        .srcAccessMask = vk::AccessFlagBits2::eTransferWrite
     };
 
     uint32_t imgIndex = m_vkCore.GetCurrentImageIndex();
-    presentToWriteBarrier.image = m_vkCore.GetSwapchain().GetSwapchainImage(static_cast<int>(imgIndex));
-    writeToPresentBarrier.image = m_vkCore.GetSwapchain().GetSwapchainImage(static_cast<int>(imgIndex));
+    const auto& swapchainImage = m_vkCore.GetSwapchain().GetSwapchainImage(static_cast<int>(imgIndex));
+    toTransferSrcBarrier.image = swapchainImage;
+
+    auto& drawImage = m_vkCore.GetDrawImage();
+
+    toGeneralBarrier.image = drawImage.image;
 
     vk::DependencyInfoKHR dependencyInfo = {
         .dependencyFlags = {},
         .imageMemoryBarrierCount = 1,
-        .pImageMemoryBarriers = &presentToWriteBarrier,
+        .pImageMemoryBarriers = &toGeneralBarrier,
     };
 
     cmdBuffer.pipelineBarrier2(dependencyInfo);
 
-    cmdBuffer.clearColorImage(m_vkCore.GetSwapchain().GetSwapchainImage(static_cast<int>(imgIndex)),
-                              vk::ImageLayout::eTransferDstOptimal, clearColor, imageRange);
+    cmdBuffer.clearColorImage(m_vkCore.GetDrawImage().image, vk::ImageLayout::eTransferDstOptimal, clearColor, imageRange);
 
     dependencyInfo = {
         .dependencyFlags = {},
@@ -123,12 +135,9 @@ void Renderer::Draw() {
 
     cmdBuffer.pipelineBarrier2(dependencyInfo);
 
+    vk::Extent2D drawExtent = {drawImage.extent.width, drawImage.extent.height};
+    ptvk::CopyImage(cmdBuffer, drawImage.image, swapchainImage, drawExtent, m_vkCore.GetSwapchain().GetExtent());
     m_vkCore.SubmitFrame();
     m_vkCore.PresentFrame();
 }
-
-void Renderer::PrepareFrameData() {
-    return;
-}
-
 }
